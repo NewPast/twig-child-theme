41f67e8d3fd35a9598e9fc2f3b3bc10f20fc8096
NewPast <chemnoor@gmail.com>
2021-05-04 22:23:19 +0300

    Update single.php

--- single.php
+++ single.php
@@ -1,1 +1,8 @@
-
+<?php 
+/** single.php 
+  * The Template for displaying all single posts 
+  */ 
+$context = Timber::context(); 
+$timber_post = Timber::get_post(); 
+$context['post'] = $timber_post; 
+Timber::render( 'single.twig', $context );
